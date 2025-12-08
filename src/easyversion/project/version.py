670a437d55cc568297563f95ad3123173ab79bd4
VantorreWannes <109341383+VantorreWannes@users.noreply.github.com>
2025-12-08 10:57:14 +0100

    fix: Fix restore method.

--- src/easyversion/project/version.py
+++ src/easyversion/project/version.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, field
+import os
 from pathlib import Path
+import shutil
 import zlib
 
 
@@ -21,11 +23,13 @@ class ProjectVersion:
     ) -> None:
         base = root_dir / sub_dir_path if sub_dir_path is not None else root_dir
         for p in sorted(
-            pp for pp in base.rglob("*") if pp.is_file() and ".ev" not in pp.parts
+            pp for pp in base.rglob("*") if pp.is_file()
         ):
             self.add_file(file_store, root_dir, p.relative_to(root_dir))
 
     def restore(self, root_dir: Path, file_store: FileStore) -> None:
+        if root_dir.exists():
+            shutil.rmtree(root_dir)
         for rel, fid in self.files.items():
             abs_path = root_dir / rel
             abs_path.parent.mkdir(parents=True, exist_ok=True)
