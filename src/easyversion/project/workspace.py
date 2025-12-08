c57d66d090e040ece39bb058413d7d13974156f7
VantorreWannes <109341383+VantorreWannes@users.noreply.github.com>
2025-12-08 10:56:59 +0100

    fix: Fix add_dir method.

--- src/easyversion/project/workspace.py
+++ src/easyversion/project/workspace.py
@@ -21,13 +21,20 @@ class ProjectWorkspace:
         )
         self.versions.append(project_version)
 
-    def split(self, new_dir: Path, version_index: int) -> ProjectWorkspace:
-        versions: list[ProjectVersion] = [
-            v.clone() for v in self.versions[:version_index]
-        ]
-        if versions:
-            last_project_version: ProjectVersion = versions[-1]
-            last_project_version.restore(new_dir, self.file_store)
+    def split(
+        self, new_dir: Path, version_index: int | None = None
+    ) -> ProjectWorkspace:
+        if version_index == -1:
+            version_slice = self.versions
+        else:
+            version_slice = self.versions[:version_index]
+
+        versions: list[ProjectVersion] = [v.clone() for v in version_slice]
+
+        last_project_version: ProjectVersion = versions[-1]
+
+        last_project_version.restore(new_dir, self.file_store)
+
         return ProjectWorkspace(new_dir, self.file_store, versions)
 
     def to_json(self) -> str:
